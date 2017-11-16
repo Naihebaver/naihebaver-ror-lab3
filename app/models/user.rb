5a4ddded4b2cd0a0565523305f27473d776eee9e
Naihebaver <vladyslav.naihebaver@gmail.com>
2017-11-16 17:43:48 +0200

    add client_side_validations to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :postcomments
 
+  validates :nickname, :email, :password, :password_confirmation, presence: true
+  validates :nickname, length: { in: 3..20 }
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/default.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
