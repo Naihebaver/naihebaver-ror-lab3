1e6cbc4abbe6d34a1e258c999e46dcad9974924a
Naihebaver <vladyslav.naihebaver@gmail.com>
2017-12-04 15:11:26 +0200

    add edit and delete comments

--- app/controllers/postcomments_controller.rb
+++ app/controllers/postcomments_controller.rb
@@ -2,10 +2,10 @@ class PostcommentsController < ApplicationController
 before_action :authenticate_user!
 
   def create
-  	params[:postcomment][:post_id]=params[:post_id]
-  	params[:postcomment][:user_id]=current_user.id
-  	@comment = Postcomment.create(comment_params)
-  	if @comment.save
+    params[:postcomment][:post_id]=params[:post_id]
+    params[:postcomment][:user_id]=current_user.id
+    @comment = Postcomment.create(comment_params)
+    if @comment.save
       redirect_to post_path(params[:post_id])
     else
       @comment.errors.full_messages.each do |msg|
@@ -17,11 +17,36 @@ before_action :authenticate_user!
   end
 
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.postcomments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.postcomments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(params[:post_id])
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.postcomments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
+
 private
-	
-	def comment_params
-		params.require(:postcomment).permit(:post_id, :user_id, :comment)
-	end
+  
+  def comment_params
+    params.require(:postcomment).permit(:post_id, :user_id, :comment)
+  end
 
 
 
