b6bac21fc3fa2b468f5447457e391864fb19e812
Naihebaver <vladyslav.naihebaver@gmail.com>
2017-12-08 16:16:52 +0200

    add photo validations and some fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-  	 @posts = Post.all.paginate(:page => params[:page], :per_page => 4)
+  	 @posts = Post.order("created_at DESC").paginate(:page => params[:page], :per_page => 4)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,6 @@ class Photo < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
 	has_many :votes
+
+	validates :photo, :name, presence: true
 end
